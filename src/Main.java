c269e5fc45fefc9cbe72415437fea27feb72f4bb
glinka <alexandraglinka@yandex.ru>
2023-03-11 20:34:32 +0300

    Task_1 + random array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    // генерируем случайные значения
+    // для этого вызываем метод generateRandomArray():
+    // значения от 100000 до 200000
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray(); // вызвали метод со случайными значениями от 100 до 200 тыс
+        //Бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //Напишите программу, которая решит эту задачу, и выведите в консоль результат в формате:
+        //«Сумма трат за месяц составила … рублей».
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 }
